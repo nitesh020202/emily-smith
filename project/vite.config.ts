a96cdf510386eee9b4a52f1f751f4d4a039f905d
nitesh020202 <nitesh020202@gmail.com>
2025-08-13 12:12:27 +0530

    Emily Smith Portfolio

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/emily-smith",
+  base:"/emilt-smith/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
